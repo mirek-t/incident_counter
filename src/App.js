bbb44bf1dd45932729ee3f9a5dcb7baf65ca1b46
Mirek Truminski <miroslaw.truminski@gmail.com>
2022-06-30 23:14:30 +0200

    counter implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Counter from "./Counter";
 import { Provider } from "react-redux";
-
 import { store } from "./store";
+import SetCounter from "./SetCounter";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Counter />
+        <SetCounter />
       </div>
     </Provider>
   );
--- src/SetCounter.jsx
+++ src/SetCounter.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useCounter } from "./useCounter";
 
 function SetCounter() {
+  const { count: countFromStore, set } = useCounter();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(countFromStore);
+  }, [countFromStore]);
+
   return (
     <div>
       <form
         onSubmit={(e) => {
-          e.preventDefault;
+          e.preventDefault();
+          set(count);
         }}
       >
-        <label htmlFor="set-to">Set Caount</label>
-        <input type="number" id="set-to" />
+        <label htmlFor="set-to">Set Count</label>
+        <input
+          type="number"
+          id="set-to"
+          value={count}
+          onChange={(e) => setCount(parseInt(e.target.value || 0, 10))}
+        />
         <input type="submit" />
       </form>
     </div>
